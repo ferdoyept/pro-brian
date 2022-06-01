9a7ad408a193b6dc47555749c84a5ef46fc5a045
ferdoyept <106685248+ferdoyept@users.noreply.github.com>
2022-06-01 10:44:28 -0500

    archivo profe brian

---
+++ profe brian.py
@@ -0,0 +1,12 @@
+
+from bs4 import BeautifulSoup 
+import requests
+
+
+result = requests.get('https://tienda.att.com.mx/outlet-en-att?')
+content = result.text
+soup = BeautifulSoup(content, 'lxml')
+item = soup.find('li' , class_= 'item product product-item outlet-en-att')
+product = item.find('a', class_= 'product-item-link').get_text()
+price =item.find('span', class_= 'price').get_text()
+print(product)
